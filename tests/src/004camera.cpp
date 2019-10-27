b472a479b1d731656270df39d34a8a9378eea45f
marko <masscry@gmail.com>
2019-10-27 23:15:23 +0300

    004camera: HUD FPS added

--- tests/src/004camera.cpp
+++ tests/src/004camera.cpp
@@ -2,6 +2,7 @@
 #include <context.hpp>
 #include <camera.hpp>
 #include <font.hpp>
+#include <text.hpp>
 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
@@ -61,21 +62,28 @@ int main(int argc, char* argv[])
 
   auto& context = bb::context_t::Instance();
   auto renderProgram = bb::shader_t::LoadProgramFromFiles("004camera_vp.glsl", "004camera_fp.glsl");
-  auto camera = bb::camera_t::Perspective(45.0f, context.Width()/(float)context.Height(), 0.1f, 100.0f);
+  auto worldCamera = bb::camera_t::Perspective(45.0f, context.Width()/(float)context.Height(), 0.1f, 100.0f);
+  auto hudCamera = bb::camera_t::Orthogonal(0.0f, context.Width(), 0.0f, context.Height());
   auto gridTexture = bb::texture_t::LoadConfig("grid.config");
+
+  bb::font_t font;
+  auto fontProgram = bb::shader_t::LoadProgramFromFiles("004camera_vp.glsl", "003font_fp.glsl");
+  font.Load("mono.config");
+  auto fpsText = bb::textDynamic_t(font, bb::vec2_t(16.0f, 32.0f));
 
   glm::vec3 pos = glm::vec3(0.0f, 0.0f, -3.0f);
   glm::vec3 dir = glm::vec3(0.0f, 0.0f, 1.0f);
   glm::vec3 up  = glm::vec3(0.0f, 1.0f, 0.0f);
 
-  bb::vbo_t vPos = bb::vbo_t::CreateArrayBuffer(quadPos, sizeof(quadPos));
-  bb::vbo_t vUV  = bb::vbo_t::CreateArrayBuffer(quadUV, sizeof(quadUV));
+  bb::vbo_t vPos = bb::vbo_t::CreateArrayBuffer(quadPos, sizeof(quadPos), false);
+  bb::vbo_t vUV  = bb::vbo_t::CreateArrayBuffer(quadUV, sizeof(quadUV), false);
   bb::vao_t quad = bb::vao_t::CreateVertexAttribObject();
 
   quad.BindVBO(vPos, 0, 3, GL_FLOAT, GL_FALSE, 0, 0);
   quad.BindVBO(vUV, 1, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-  auto bindPoint = renderProgram.UniformBlockIndex("camera");
+  auto bindPoint    = renderProgram.UniformBlockIndex("camera");
+  auto bindPointHUD = fontProgram.UniformBlockIndex("camera"); 
 
   auto lastTick = glfwGetTime();
 
@@ -87,13 +95,13 @@ int main(int argc, char* argv[])
     auto nowTick = glfwGetTime();
     auto delta = nowTick - lastTick;
 
-    bb::shader_t::Bind(renderProgram);
     bb::framebuffer_t::Bind(context.Canvas());
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    camera.Update();
-    renderProgram.SetBlock(bindPoint, camera.UniformBlock());
+    bb::shader_t::Bind(renderProgram);
+    worldCamera.Update();
+    renderProgram.SetBlock(bindPoint, worldCamera.UniformBlock());
 
     bb::vao_t::Bind(quad);
     bb::texture_t::Bind(gridTexture);
@@ -102,6 +110,16 @@ int main(int argc, char* argv[])
     glDrawArrays(GL_TRIANGLES, 0, 6);
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
+
+    bb::shader_t::Bind(fontProgram);
+    hudCamera.Update();
+    fontProgram.SetBlock(bindPointHUD, hudCamera.UniformBlock());
+
+    char buffer[64];
+    snprintf(buffer, sizeof(buffer), "FPS: %5.2f", 1.0/delta);
+
+    fpsText.Update(buffer);
+    fpsText.Render();
 
     if (!context.Update())
     {
@@ -117,7 +135,7 @@ int main(int argc, char* argv[])
     float side = (context.IsKeyDown(GLFW_KEY_D) - context.IsKeyDown(GLFW_KEY_A))*delta;
     pos += glm::cross(dir, up)*side;
 
-    camera.View() = glm::translate(glm::mat4(1.0f), pos);
+    worldCamera.View() = glm::translate(glm::mat4(1.0f), pos);
     lastTick = nowTick;
   }
 
