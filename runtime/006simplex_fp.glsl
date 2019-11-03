b6b5f89455c25551da9442f4fec71008d69752e5
marko <masscry@gmail.com>
2019-11-04 02:40:28 +0300

    006simplex: shader update

--- runtime/006simplex_fp.glsl
+++ runtime/006simplex_fp.glsl
@@ -7,10 +7,10 @@ in vec2 fragUV;
 
 uniform sampler2D mainTex;
 
-uniform float time;
+const vec4 marineBlue = vec4(0.52f, 0.65f, 0.75f, 1.0f);
 
 void main()
 {
-  vec3 texColor = texture(mainTex, fragUV).rrr;
-  pixColor = vec4(texColor, 1.0f);
+  vec3 texColor = vec3(round(texture(mainTex, fragUV).r*20.0f)/20.0f);
+  pixColor = vec4(texColor, 1.0f)*marineBlue;
 }
