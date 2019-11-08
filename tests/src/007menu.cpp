b6c4daad4c8e665a80ec0139e6df83d0dc183a0d
marko <masscry@gmail.com>
2019-11-09 01:16:39 +0300

    007menu: actor added

--- tests/src/007menu.cpp
+++ tests/src/007menu.cpp
@@ -7,7 +7,60 @@
 
 #include <common.hpp>
 #include <context.hpp>
-#include <camera.hpp>
+
+#include <actor.hpp>
+#include <worker.hpp>
+
+enum menuLine_t
+{
+  ML_FIRST = 0,
+  ML_NEW_GAME = ML_FIRST,
+  ML_LOAD_GAME,
+  ML_ABOUT,
+  ML_EXIT,
+  ML_LAST = ML_EXIT,
+  ML_TOTAL
+};
+
+enum menuMsg_t
+{
+  MT_FIRST = 0,
+  MT_PREV = MT_FIRST,
+  MT_NEXT,
+  MT_ANIMATION_START,
+  MT_ANIMATION_DONE,
+  MT_LAST = MT_ANIMATION_DONE,
+  MT_TOTAL
+};
+
+bb::mailbox_t renderTasks;
+
+class menuModel_t: public bb::actor_t
+{
+  int selected;
+
+  void ProcessMessage(bb::msg_t msg) override
+  {
+    int nextMenuLine = this->selected;
+
+    switch (msg.type)
+    {
+      case MT_PREV:
+        nextMenuLine = (((nextMenuLine-1) < ML_FIRST)?(ML_LAST):(nextMenuLine-1));
+        break;
+      case MT_NEXT:
+        nextMenuLine = (((nextMenuLine+1) > ML_LAST)?(ML_FIRST):(nextMenuLine+1));
+        break;
+      case MT_ANIMATION_DONE:
+        break;
+      default:
+        assert(0);
+    }
+  }
+
+public:
+
+};
 
 int main(int argc, char* argv[])
 {
@@ -17,6 +70,8 @@ int main(int argc, char* argv[])
   }
 
   auto& context = bb::context_t::Instance();
+  auto& pool = bb::workerPool_t::Instance();
+  int menuActor = pool.Register(std::unique_ptr<bb::actor_t>(new menuModel_t));
 
   auto renderProgram = bb::shader_t::LoadProgramFromFiles(
     "007menu_vp.glsl",
@@ -47,6 +102,8 @@ int main(int argc, char* argv[])
     }
     context.Title(std::to_string(1.0/delta));
   }
+
+  pool.Unregister(menuActor);
 
   return 0;
 }
