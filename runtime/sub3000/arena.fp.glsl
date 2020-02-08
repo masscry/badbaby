11a2af561e5899c87047981366eb26d8392a5ad4
marko <masscry@gmail.com>
2020-02-08 14:00:45 +0300

    sub3000: better ring shader

--- runtime/sub3000/arena.fp.glsl
+++ runtime/sub3000/arena.fp.glsl
@@ -3,7 +3,9 @@
 
 layout(location = 0) out vec4 pixColor;
 
+in float color;
+
 void main()
 {
-  pixColor = vec4(0.1f, 1.0f, 0.1f, 1.0f);
+  pixColor = vec4(0.1f, 1.0f, 0.1f, 1.0f)*color;
 }
--- runtime/sub3000/arena.vp.glsl
+++ runtime/sub3000/arena.vp.glsl
@@ -2,8 +2,12 @@
 #version 330 core
 
 layout(location = 0) in vec2 pos;
+layout(location = 1) in float dist;
+
+out float color;
 
 void main()
 {
+  color = dist;
   gl_Position = vec4(pos, 0.0f, 1.0f);
 }
