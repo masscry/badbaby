b7caefb06ec97e4482ccd02419aaa77366b7ef4d
timur <t.zharaspaev@gpa.ru>
2020-02-17 17:47:47 +0300

    sub3000: fix shader and mesh

--- runtime/sub3000/arena.fp.glsl
+++ runtime/sub3000/arena.fp.glsl
@@ -5,10 +5,10 @@ layout(location = 0) out vec4 pixColor;
 
 in float fragPos;
 
-const vec4 lineColor = vec4(0.1f, 1.0f, 0.1f, 1.0f);
+const vec4 lineColor = vec4(0.1f, 1.3f, 0.1f, 1.0f);
 
 void main()
 {
   float fragDist = 1.0-2.0*abs(fragPos - 0.5);
-  pixColor = lineColor*fragDist;
+  pixColor = mix(vec4(0.0f), lineColor, fragDist);
 }
--- runtime/sub3000/arena.vp.glsl
+++ runtime/sub3000/arena.vp.glsl
@@ -4,10 +4,16 @@
 layout(location = 0) in vec2 pos;
 layout(location = 1) in float dist;
 
+uniform camera
+{
+  mat4 proj;
+  mat4 view;
+};
+
 out float fragPos;
 
 void main()
 {
   fragPos = dist;
-  gl_Position = vec4(pos, 0.0f, 1.0f);
+  gl_Position = proj * view * vec4(pos, 0.0f, 1.0f);
 }
