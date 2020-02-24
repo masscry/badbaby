a7d0feb4c045cb3bec92d983c24ce05bfa996aa9
marko <masscry@gmail.com>
2020-02-25 01:06:07 +0300

    actor: mailbox refresh

--- core/actor/include/mailbox.hpp
+++ core/actor/include/mailbox.hpp
@@ -12,6 +12,8 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
+#include <unordered_map>
+#include <memory>
 
 #include <msg.hpp>
 
@@ -20,11 +22,14 @@ namespace bb
 
   class mailbox_t final
   {
+    friend class postOffice_t;
+
     using storage_t = std::queue<msg_t>;
 
-    std::mutex guard;
-    std::condition_variable notify;
-    storage_t  storage;
+    mutable std::mutex guard;
+    mutable std::condition_variable notify;
+    storage_t storage;
+    uint32_t address;
 
     mailbox_t(const mailbox_t&) = delete;
     mailbox_t(mailbox_t&&) = delete;
@@ -32,11 +37,18 @@ namespace bb
     mailbox_t& operator=(const mailbox_t&) = delete;
     mailbox_t& operator=(mailbox_t&&) = delete;
 
+    explicit mailbox_t(uint32_t address);
+
   public:
 
-    size_t Has();
+    using shared_t = std::shared_ptr<mailbox_t>;
+    using weak_t = std::weak_ptr<mailbox_t>;
 
-    bool Empty();
+    uint32_t Address() const;
+
+    size_t Has() const;
+
+    bool Empty() const;
 
     msg_t Wait();
 
@@ -44,10 +56,58 @@ namespace bb
 
     void Put(msg_t&& msg);
 
-    mailbox_t() = default;
-    ~mailbox_t() = default;
+    ~mailbox_t();
 
   };
+
+  class postOffice_t final
+  {
+    friend class mailbox_t;
+
+    using storage_t = std::unordered_map<uint32_t, mailbox_t::weak_t>;
+
+    std::mutex guard;
+    storage_t storage;
+
+    postOffice_t() = default;
+    ~postOffice_t() = default;
+
+    postOffice_t(const postOffice_t&) = delete;
+    postOffice_t& operator=(const postOffice_t&) = delete;
+    postOffice_t(postOffice_t&&) = delete;
+    postOffice_t& operator=(postOffice_t&&) = delete;
+
+    void Delete(uint32_t address);
+
+    mailbox_t::shared_t New(uint32_t address);
+
+  public:
+
+    static postOffice_t& Instance();
+
+    mailbox_t::shared_t New(const std::string& address);
+
+    int Post(const std::string& address, msg_t&& msg);
+    int Post(uint32_t address, msg_t&& msg);
+
+  };
+
+  inline uint32_t mailbox_t::Address() const
+  {
+    return this->address;
+  }
+
+  inline size_t mailbox_t::Has() const
+  {
+    std::unique_lock<std::mutex> lock(this->guard);
+    return this->storage.size();
+  }
+
+  inline bool mailbox_t::Empty() const
+  {
+    std::unique_lock<std::mutex> lock(this->guard);
+    return this->storage.empty();
+  }
 
 }
 
