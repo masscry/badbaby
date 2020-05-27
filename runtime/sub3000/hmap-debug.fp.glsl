960cb9de43962ed0c9615c7bc9000e9e13b86155
marko <masscry@gmail.com>
2020-05-28 00:55:14 +0300

    sub3000: better visuals

--- runtime/sub3000/hmap-debug.fp.glsl
+++ runtime/sub3000/hmap-debug.fp.glsl
@@ -8,21 +8,30 @@ in vec2 projView;
 
 uniform sampler2D mainTex;
 
-const vec3 black = vec3(0.0);
-const vec3 water = vec3(0.0, 0.1f, 0.0);
-const vec3 land = vec3(0.1f, 0.5f, 0.1f);
+const vec3 black = vec3(0.1f, 0.1f, 0.1f);
+const vec3 water = vec3(0.0, 0.2f, 0.1f);
+const vec3 land = vec3(0.3f, 0.3f, 0.1f);
 
 uniform float border;
+
+float edgeDetector(float z)
+{
+  z *= 192.0f;
+  float d = fract(z);
+  d = mix(d, 1.0f-d, step(mod(z, 2.0f), 1.0f));
+  return d/fwidth(z)*0.5f;
+}
 
 void main()
 {
   float fdepth = texture(mainTex, fragUV).r;
 
-  vec3 waterColor = mix(black, water, fdepth/border);
-  vec3 landColor = mix(black, land, fdepth/(1.0f-border));
+  float edge = 1.0f - edgeDetector(fdepth);
 
-  float stepVal = 1.0 - step(border, fdepth);
-  vec3 coldepth = waterColor*stepVal + landColor*(1.0 - stepVal); 
+  vec3 waterColor = mix(black, water, fdepth/border)*max(0.5f, edge);
+  vec3 landColor = mix(black, land, fdepth/(1.0f-border))*max(0.5f, edge);
+
+  vec3 coldepth = mix(waterColor, landColor, step(border, fdepth)); 
 
   float dst = (1.0f-smoothstep(0.90f, 0.95f, length(projView)));
   outColor = vec4(coldepth*dst, dst);
